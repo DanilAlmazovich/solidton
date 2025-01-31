712591944d590638850813f75aab818c546e39a0
Данил Бузурманалиев <dbuzurmanaliev@srs.lan>
2025-01-31 15:26:17 +0600

    Fix

--- src/views/Example.vue
+++ src/views/Example.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import {Vue3Lottie} from 'vue3-lottie'
-// import data1 from "@/animation-example/1.json";
+import data1 from "@/animation-example/1.json";
 import data2 from "@/animation-example/2.json";
-// import data3 from "@/animation-example/3.json";
+import data3 from "@/animation-example/3.json";
 import data4 from "@/animation-example/4.json";
 import data5 from "@/animation-example/5.json";
-// import data6 from "@/animation-example/6.json";
-// import data7 from "@/animation-example/7.json";
-// import data9 from "@/animation-example/9.json";
+import data6 from "@/animation-example/6.json";
+import data7 from "@/animation-example/7.json";
+import data9 from "@/animation-example/9.json";
 // import data10 from "@/assets/animations/10.json";
 import data11 from "@/animation-example/11.json";
 import data12 from "@/animation-example/12.json";
@@ -17,48 +17,45 @@ import data13 from "@/animation-example/11 (2).json";
 <template>
   <div class="container">
     <div class="grid grid-cols-2 gap-8">
-<!--      <div>-->
-<!--        <Vue3Lottie loop :animation-data="data" height="100%" width="100%"/>-->
-<!--      </div>-->
+      <div>
+        <Vue3Lottie loop :animation-data="data1" height="100%" width="100%"/>
+      </div>
       <div>
         <Vue3Lottie loop :animation-data="data2" height="100%" width="100%"/>
       </div>
 <!--      <div>-->
 <!--        <Vue3Lottie loop :animation-data="data2" height="100%" width="100%"/>-->
 <!--      </div>-->
-<!--      <div>-->
-<!--        <Vue3Lottie loop :animation-data="data3" height="100%" width="100%"/>-->
-<!--      </div>-->
-<!--      <div>-->
-<!--        <Vue3Lottie loop :animation-data="data4" height="100%" width="100%"/>-->
-<!--      </div>-->
-<!--      <div>-->
-<!--        <Vue3Lottie loop :animation-data="data5" height="100%" width="100%"/>-->
-<!--      </div>-->
-<!--      <div>-->
-<!--        <Vue3Lottie loop :animation-data="data6" height="100%" width="100%"/>-->
-<!--      </div>-->
-<!--      <div>-->
-<!--        <Vue3Lottie loop :animation-data="data7" height="100%" width="100%"/>-->
-<!--      </div>-->
-<!--      <div>-->
-<!--        <Vue3Lottie loop :animation-data="data8" height="100%" width="100%"/>-->
-<!--      </div>-->
-<!--      <div>-->
-<!--        <Vue3Lottie loop :animation-data="data9" height="100%" width="100%"/>-->
-<!--      </div>-->
+      <div>
+        <Vue3Lottie loop :animation-data="data3" height="100%" width="100%"/>
+      </div>
+      <div>
+        <Vue3Lottie loop :animation-data="data4" height="100%" width="100%"/>
+      </div>
+      <div>
+        <Vue3Lottie loop :animation-data="data5" height="100%" width="100%"/>
+      </div>
+      <div>
+        <Vue3Lottie loop :animation-data="data6" height="100%" width="100%"/>
+      </div>
+      <div>
+        <Vue3Lottie loop :animation-data="data7" height="100%" width="100%"/>
+      </div>
+      <div>
+        <Vue3Lottie loop :animation-data="data11" height="100%" width="100%"/>
+      </div>
+      <div>
+        <Vue3Lottie loop :animation-data="data9" height="100%" width="100%"/>
+      </div>
 <!--      <div>-->
 <!--        <Vue3Lottie loop :animation-data="data10" height="100%" width="100%"/>-->
 <!--      </div>-->
-<!--      <div>-->
-<!--        <Vue3Lottie loop :animation-data="data12" height="100%" width="100%"/>-->
-<!--      </div>-->
-<!--      <div>-->
-<!--        <Vue3Lottie loop :animation-data="data13" height="100%" width="100%"/>-->
-<!--      </div>-->
-<!--      <div>-->
-<!--        <Vue3Lottie loop :animation-data="data14" height="100%" width="100%"/>-->
-<!--      </div>-->
+      <div>
+        <Vue3Lottie loop :animation-data="data12" height="100%" width="100%"/>
+      </div>
+      <div>
+        <Vue3Lottie loop :animation-data="data13" height="100%" width="100%"/>
+      </div>
 <!--      <div>-->
 <!--        <Vue3Lottie loop :animation-data="data15" height="100%" width="100%"/>-->
 <!--      </div>-->
